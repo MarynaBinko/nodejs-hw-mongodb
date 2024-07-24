deb62e18a706654461d9f534e4f2ba7107587a4f
Maryna Binko <benyhorses@hotmail.com>
2024-07-24 12:24:28 +0100

    string

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const initMongoConnection = async () => {
   const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
-  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+  
+  const MONGODB_CONNECTION_STRING = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
 
   try {
-    await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import { setupServer } from './server.js';
 import initMongoConnection from './db/initMongoConnection.js';
 import dotenv from 'dotenv';

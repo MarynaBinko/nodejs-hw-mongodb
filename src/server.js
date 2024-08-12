8248459a961c646039f59502e6d7c5b93fe5b6d9
Maryna Binko <benyhorses@hotmail.com>
2024-08-12 19:49:14 +0100

    auth

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
@@ -10,16 +11,22 @@ const setupServer = () => {
   app.use(morgan('dev'));
   app.use(express.json());
 
+
+  app.use('/auth', authRouter);
+
+
   app.use('/contacts', contactsRouter);
 
-  // Handle 404 errors
+
   app.use(notFoundHandler);
 
-  // Error handling middleware
+
   app.use(errorHandler);
 
   return app;
 };
 
 export { setupServer };
+
+
 

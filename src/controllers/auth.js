b8ae6a0a241262130743092ea1e4cacdac46744d
Maryna Binko <benyhorses@hotmail.com>
2024-08-16 13:26:16 +0100

    refresh

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -23,6 +23,10 @@ export const loginUser = async (req, res, next) => {
     const { email, password } = req.body;
     const { accessToken, refreshToken } = await loginUserService({ email, password });
 
+    if (!accessToken || !refreshToken) {
+      throw new Error('Failed to generate tokens');
+    }
+
     res.cookie('refreshToken', refreshToken, { httpOnly: true });
     res.status(200).json({
       status: 200,

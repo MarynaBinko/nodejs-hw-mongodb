962b3a91e5d3ddcfaa30ac3d741a704db7114841
Maryna Binko <benyhorses@hotmail.com>
2024-08-28 13:04:11 +0100

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import { setupServer } from './server.js';
 import initMongoConnection from './db/initMongoConnection.js';
 import dotenv from 'dotenv';
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 dotenv.config();
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const startApp = async () => {
   try {
-    // Initialize MongoDB connection
+
     await initMongoConnection();
 
-    // Set up the server
+
+    const swaggerPath = path.resolve(__dirname, '../docs/swagger.json');
+    const swaggerFile = await fs.readFile(swaggerPath, 'utf8');
+    const swaggerDocument = JSON.parse(swaggerFile);
+
     const app = setupServer();
 
-    // Start the server
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+   
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
@@ -27,10 +38,5 @@ const startApp = async () => {
 };
 
 startApp();
-
-
-
-
-
 
 

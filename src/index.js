3478e71df77527e5c6e324026e0de07bb89c3815
Maryna Binko <benyhorses@hotmail.com>
2024-08-29 19:03:38 +0100

    test

--- src/index.js
+++ src/index.js
@@ -5,27 +5,27 @@ import swaggerUi from 'swagger-ui-express';
 import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import yaml from 'js-yaml';
 
 dotenv.config();
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const startApp = async () => {
   try {
-
     await initMongoConnection();
 
-    const swaggerPath = path.resolve(__dirname, '../docs/swagger.json');
+    const swaggerPath = path.resolve(__dirname, '../docs/openapi.yaml');
+    console.log(`Swagger file path: ${swaggerPath}`);
     const swaggerFile = await fs.readFile(swaggerPath, 'utf8');
-    const swaggerDocument = JSON.parse(swaggerFile);
+    console.log(`Swagger file content: ${swaggerFile}`);
+    const swaggerDocument = yaml.load(swaggerFile);
+    console.log('Swagger document parsed successfully');
 
     const app = setupServer();
 
-
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
